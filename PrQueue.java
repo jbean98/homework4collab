d6e144e01db5c430d4cfa89b432a036dbc992072
dorothyquincy <dorothyq1216@gmail.com>
2017-07-22 21:14:28 -0400

    Add files via upload

---
+++ PrQueue.java
@@ -0,0 +1,9 @@
+/**
+ * Created by dorothyquincy on 22/07/2017.
+ */
+
+public class PrQueue {
+    public static void main(String[] args) {
+        System.out.println("priority queue class");
+    }
+}
